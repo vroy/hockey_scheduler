7e915c0aabbca2d60a02003271ed604d781996ac
Vincent Roy <VincentRoy8@gmail.com>
2010-06-05 11:11:50 -0300

    Improve the time dropdown to select noon by default and change the name of Noon and Midnight to make it more straightforward.

--- helper/dropdown.rb
+++ helper/dropdown.rb
@@ -1,13 +1,19 @@
 module Ramaze::Helper::Dropdown
   
-  def time_dropdown(step=15, key=:timestamp, selected=nil) # step in minutes
+  def time_dropdown(key=:timestamp, step=15, selected=nil) # step in minutes
     values = []
     start_time = Time.parse(Date.today.to_s)
     end_time = Time.parse((Date.today+1).to_s)
     
     while start_time < end_time
-      display = start_time.strftime("%H:%M")
-      values << display
+      value = start_time.strftime("%H:%M")
+      
+      display = value
+      display = "Midnight" if value == "00:00"
+      display = "Noon" if value == "12:00"
+      
+      values << [value, display]
+      
       start_time += step * 60
     end
     
@@ -16,8 +22,10 @@ module Ramaze::Helper::Dropdown
     html = ""
     html << "<select id='#{key}' name='#{key}'>"
     
-    values.each do |value|
-      html << "<option value='#{value}'>#{value}</option>"
+    values.each do |value, display|
+      selected = (value=="12:00") ? " selected='selected' " : ""
+      
+      html << "<option value='#{value}' #{selected}>#{display}</option>"
     end
     
     html << "</select>"
